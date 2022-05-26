b7bb9f1cee3de8a2c3b27d4f2fa4841adb7acd28
Andrea Pasquale <andreapasquale97@gmail.com>
2022-05-26 16:44:42 +0200

    update training

--- less_pixel_simplified.py
+++ less_pixel_simplified.py
@@ -55,9 +55,9 @@ class Averager:
 
 # define the standalone discriminator model
 def define_discriminator(n_inputs=16, alpha=0.2, dropout=0.2, lr=0.1):
-    model = Sequential()     
+    model = Sequential()
     model.add(Dense(200, use_bias=False, input_dim=n_inputs))
-    model.add(Reshape((10,10,2)))  
+    model.add(Reshape((10,10,2)))
     model.add(Conv2D(64, kernel_size=3, strides=1, padding='same', kernel_initializer='glorot_normal'))
     model.add(LeakyReLU(alpha=alpha))
     model.add(Conv2D(32, kernel_size=3, strides=1, padding='same', kernel_initializer='glorot_normal'))
@@ -67,15 +67,15 @@ def define_discriminator(n_inputs=16, alpha=0.2, dropout=0.2, lr=0.1):
     model.add(Conv2D(8, kernel_size=3, strides=1, padding='same', kernel_initializer='glorot_normal'))
     model.add(Flatten())
     model.add(LeakyReLU(alpha=alpha))
-    model.add(Dropout(dropout)) 
-    
+    model.add(Dropout(dropout))
+
     model.add(Dense(1, activation='sigmoid'))
 
     # compile model
     opt = Adadelta(learning_rate=lr)
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
     return model
- 
+
 # define the combined generator and discriminator model, for updating the generator
 def define_cost_gan(params, discriminator, latent_dim, samples, circuit, nqubits, layers, pixels):
     # generate fake samples
@@ -92,22 +92,23 @@ def set_params(circuit, params, x_input, i, nqubits, layers, latent_dim):
     p = []
     index = 0
     noise = 0
-    for l in range(layers):
+    for q in range(nqubits):
+        p.append(params[index] * x_input[noise][i] + params[index+1])
+        index += 2
+        noise= (noise+1) % latent_dim
+    for q in range(nqubits):
+        p.append(params[index])
+        index += 1
+    for l in range(1,layers):
         for q in range(nqubits):
-            if l == 0:
-                p.append(params[index] * x_input[noise][i] + params[index+1])
-                index += 2
-                noise= (noise+1) % latent_dim
-            else:
-                p.append(params[index])
-                index += 1
+            p.append(params[index])
+            index += 1
             p.append(params[index])# * x_input[noise][i] + params[index+1])
             index += 1
             #noise = (noise+1) % latent_dim
     for q in range(nqubits):
         p.append(params[index])# * x_input[noise][i] + params[index+1])
         index += 1
-        #noise= (noise+1) % latent_dim
     circuit.set_parameters(p)
 
 def generate_training_real_samples(dataset, samples, avg=1):
@@ -182,7 +183,7 @@ def generate_latent_points(latent_dim, samples):
     # reshape into a batch of inputs for the network
     x_input = x_input.reshape(samples, latent_dim)
     return x_input
- 
+
 # use the generator to generate fake examples, with class labels
 def generate_fake_samples(params, latent_dim, samples, circuit, nqubits, layers, pixels):
     # generate points in latent space
@@ -211,7 +212,7 @@ def train(d_model, latent_dim, layers, nqubits, training_samples, circuit, n_epo
     g_loss = []
     # determine half the size of one batch, for updating the discriminator
     half_samples = int(samples / 2)
-    initial_params = tf.Variable(np.random.uniform(-0.15, 0.15, 3*nqubits+2*(layers-1)*nqubits + 2*nqubits))
+    initial_params = tf.Variable(np.random.uniform(0, 2*np.pi, 3*nqubits+2*(layers-1)*nqubits + nqubits))
     optimizer = tf.optimizers.Adadelta(learning_rate=lr)
     # prepare real samples
     s, pixels = generate_training_real_samples(dataset, training_samples, avg)
@@ -245,7 +246,7 @@ def train(d_model, latent_dim, layers, nqubits, training_samples, circuit, n_epo
 
 def build_and_train_model(lr_d=1e-2, lr=1e-2, n_epochs=10, batch_samples=10, latent_dim=10, layers=1,
                           training_samples=200, pixels=16, nqubits=4, avg=1, dataset=None, folder=None):
-    
+
     # number of qubits generator
     nqubits = nqubits
     # create quantum generator
